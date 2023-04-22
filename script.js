fa615b7f35f87f871326e1b0ad278de867e043c4
Sakshi Gupta <43090350+skshig@users.noreply.github.com>
2023-04-22 20:58:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
-function calculateMinCost() {
-  //your code here
-	let arr = [];
-	let n = arr.length;
-	let pq = [];
-   
-        // Adding items to the pQueue
-        for (let i = 0; i < n; i++) {
-            pq.push(arr[i]);
-        }   
-           
-        pq.sort(function(a,b){return a-b;});
-         
-        // Initialize result
-        let res = 0;
-   
-        // While size of priority queue
-        // is more than 1
-        while (pq.length > 1) {
-            // Extract shortest two ropes from pq
-            let first = pq.shift();
-            let second = pq.shift();
-   
-            // Connect the ropes: update result
-            // and insert the new rope to pq
-            res += first + second;
-            pq.push(first + second);
-            pq.sort(function(a,b){return a-b;});
-        }
-   
-        return res;
-  
-  
-  
-}  
+const input = document.getElementById("input");
+const submit = document.getElementById("submit");
+const result = document.getElementById("result");
+
+submit.addEventListener("click", function() {
+// Split the input string into an array of strings
+const ropes = input.value.split(',');
+
+// Convert the array of strings into an array of numbers
+const ropesArray = ropes.map(rope => parseInt(rope));
+
+// Sort the array in ascending order
+ropesArray.sort((a, b) => a - b);
+
+// Calculate the minimum cost of ropes
+let cost = 0;
+while (ropesArray.length > 1) {
+const first = ropesArray.shift();
+const second = ropesArray.shift();
+cost += first + second;
+ropesArray.unshift(first + second);
+ropesArray.sort((a, b) => a - b);
+}
+
+// Display the minimum cost of ropes
+result.innerHTML = `Minimum cost of ropes: ${cost}`;
+});
